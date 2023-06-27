2ad7c187453aaecbbe00578fe953f27e3b7bdd8a
OnnoKoreneef <onno.koreneef@student.hu.nl>
2023-06-27 11:51:06 +0200

    registreren alles moet ingevuld zijn

--- src/main/webApp/javascript/registreren.js
+++ src/main/webApp/javascript/registreren.js
@@ -15,58 +15,75 @@ async function registreren(event) {
         wachtwoord: registrerenForm.wachtwoord.value,
     }
 
+    console.log(data);
+
     const email = data.email;
     const wachtwoord = data.wachtwoord;
     const herhaalWachtwoord = registrerenForm.herWachtwoord.value;
 
-    if (wachtwoord.length < 8) {
-        alert("Wachtwoord moet minstens 8 karakters zijn");
-        registrerenForm.wachtwoord.value = "";
-        registrerenForm.herWachtwoord.value = "";
-    } else if (containsNumber(wachtwoord) === false || containsSpecialCharacter(wachtwoord) === false || containsUppercase(wachtwoord) === false || containsLowercase(wachtwoord) === false) {
-        alert("Wachtwoord moet een hoofdletter, een cijfer en een speciaal teken bevatten");
-        registrerenForm.wachtwoord.value = "";
-        registrerenForm.herWachtwoord.value = "";
-    } else if (wachtwoord !== herhaalWachtwoord) {
-        alert("Wachtwoord en herhaal wachtwoord zijn niet hetzelfde");
+    if(isIngevuld(data, herhaalWachtwoord) === false) {
+        alert("Niet alles is ingevuld");
         registrerenForm.wachtwoord.value = "";
         registrerenForm.herWachtwoord.value = "";
     } else {
-        const url = "http://localhost:8080/restservices/klanten";
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        }
+        if (wachtwoord.length < 8) {
+            alert("Wachtwoord moet minstens 8 karakters zijn");
+            registrerenForm.wachtwoord.value = "";
+            registrerenForm.herWachtwoord.value = "";
+        } else if (containsNumber(wachtwoord) === false || containsSpecialCharacter(wachtwoord) === false || containsUppercase(wachtwoord) === false || containsLowercase(wachtwoord) === false) {
+            alert("Wachtwoord moet een hoofdletter, een cijfer en een speciaal teken bevatten");
+            registrerenForm.wachtwoord.value = "";
+            registrerenForm.herWachtwoord.value = "";
+        } else if (wachtwoord !== herhaalWachtwoord) {
+            alert("Wachtwoord en herhaal wachtwoord zijn niet hetzelfde");
+            registrerenForm.wachtwoord.value = "";
+            registrerenForm.herWachtwoord.value = "";
+        } else {
+            const url = "http://localhost:8080/restservices/klanten";
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            }
 
-        await fetch(url, options)
-            .then((response) => response.json())
-            .then((data) => {
-                window.location.href = `../pages/gegevens.html?email=${encodeURIComponent(email)}`;
-                console.log(data);
-            }).catch((error) => {
-                console.log(error);
-            })}
-
-    function containsNumber(string) {
-        const pattern = /\d/;
-        return pattern.test(string);
+            await fetch(url, options)
+                .then((response) => response.json())
+                .then((data) => {
+                    window.location.href = `../pages/gegevens.html?email=${encodeURIComponent(email)}`;
+                    console.log(data);
+                }).catch((error) => {
+                    console.log(error);
+                })}
     }
+}
 
-    function containsSpecialCharacter(string) {
-        const pattern = /[!@#$%^&*(),.?":{}|<>]/;
-        return pattern.test(string);
-    }
+function containsNumber(string) {
+    const pattern = /\d/;
+    return pattern.test(string);
+}
 
-    function containsLowercase(string) {
-        const pattern = /[a-z]/;
-        return pattern.test(string);
-    }
+function containsSpecialCharacter(string) {
+    const pattern = /[!@#$%^&*(),.?":{}|<>]/;
+    return pattern.test(string);
+}
 
-    function containsUppercase(string) {
-        const pattern = /[A-Z]/;
-        return pattern.test(string);
+function containsLowercase(string) {
+    const pattern = /[a-z]/;
+    return pattern.test(string);
+}
+
+function containsUppercase(string) {
+    const pattern = /[A-Z]/;
+    return pattern.test(string);
+}
+
+function isIngevuld(gegevens, herhaalWachtwoord) {
+    let ingevuld = false;
+    if (gegevens.voornaam !== "" && gegevens.achternaam !== "" && gegevens.woonplaats !== "" && gegevens.straatnaam !== ""
+        && gegevens.huisNummer  !== "" && gegevens.telefoonnummer !== "" && gegevens.email !== "") {
+        ingevuld = true;
     }
+    return ingevuld;
 }
